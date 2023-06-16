5ceeecaa2b34338b0a79d9d5df6ec11015e1449b
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-06-16 22:52:03 +0200

    first commit of service

---
+++ service/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+</head>
+<body>
+    <header>
+        <img src="../img/Sous-titre.png" alt="logo Consisto">
+
+        <nav class="nav__bar">
+            <ul>
+                <li>Destination</li>
+                <li>My reservation</li>
+                <li>Sign up</li>
+            </ul>
+        </nav>
+    </header>
---
+++ service/index.php
@@ -0,0 +1,3 @@
+<?php include_once "header.php";?>
+    <h1>Manger</h1>
+<?php include_once "footer.php";?>
