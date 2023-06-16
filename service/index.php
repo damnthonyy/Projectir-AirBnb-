fdf0f8ca190d1bebb042b185d6f9083ef3ac3c9f
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-06-17 00:57:00 +0200

    update taches
    a revoir le style

--- service/index.php
+++
@@ -1,3 +0,0 @@
-<?php include_once "header.php";?>
-    <h1>Manger</h1>
-<?php include_once "footer.php";?>
---
+++ service/tache.php
@@ -0,0 +1,18 @@
+<?php include_once "header.php";?>
+
+    <section>
+        <img class="img" src="../service/img/707fb53d718963d3eb6e6636e9f56d35.jpg" alt="">
+        <h1>Taches à faire</h1>
+
+        <div class="statut">
+            <h1>Le 01/01/2023 - 07/07/2023</h1>
+            <p>Statue : en cours</p>
+        </div>
+        <hr>
+
+        <div class="adresse">
+            <h3>Adresse</h3>
+            <p>Amet Consecteur Adipiscing Elit Pellentesque</p>
+        </div>
+    </section>
+<?php include_once "footer.php";?>
