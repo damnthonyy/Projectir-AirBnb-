78eaaeccb4dde862e733b5acc89b7ff4103a61b3
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-06-18 16:53:00 +0200

    Update cookie

--- sendmail/cookies.php
+++ sendmail/cookies.php
@@ -4,16 +4,17 @@ session_start();
 // Après une connexion réussie
 
 $_SESSION['username'];
-setcookie('session_id', session_id(), time() + 60, '/');
+setcookie('session_id', session_id(), time() + 120 , '/');
 
 if (isset($_COOKIE['session_id']) && !empty($_COOKIE['session_id'])) {
-    // session_id($_COOKIE['session_id']);
+    
     // Récupère les informations de session
-    // $user_id = $_SESSION['user_id'];
+    $user_id = $_SESSION['user_id'];
     $username = $_SESSION['username'];
-    // Autres traitements...
+    
 } else {
     // L'utilisateur n'est pas connecté
+    //on le rédirige vers la page de connexion
     header("Location: ../sendmail/index.php");
 }
 
