278219a2c1564f5f68dc69803d1f499c0d903b79
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-06-18 23:14:30 +0200

    Update bdd

--- sendmail/bdd.php
+++ sendmail/bdd.php
@@ -8,10 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-<?php echo "<h1>Bonjour " . $_SESSION["username"] . ", vous êtes connecté</h1>"; ?>
+
+<?php echo "<h1>Bonjour " . htmlspecialchars($_SESSION["username"] ). ", vous êtes connecté</h1>"; ?>
 
 </body>
-
 </html>
--- sendmail/cookies.php
+++ sendmail/cookies.php
@@ -3,8 +3,10 @@ session_start();
 
 // Après une connexion réussie
 
-$_SESSION['username'];
-setcookie('session_id', session_id(), time() + 120 , '/');
+$user_id = $_SESSION['user_id'];
+$username = $_SESSION['username'];
+
+setcookie('session_id', session_id(), time() + 60, '/');
 
 if (isset($_COOKIE['session_id']) && !empty($_COOKIE['session_id'])) {
     
@@ -13,9 +15,10 @@ if (isset($_COOKIE['session_id']) && !empty($_COOKIE['session_id'])) {
     $username = $_SESSION['username'];
     
 } else {
+
     // L'utilisateur n'est pas connecté
     //on le rédirige vers la page de connexion
     header("Location: ../sendmail/index.php");
+    unset($_COOKIE);
+    // setcookie('session_id', '', time()- 10 , '/');
 }
-
-
